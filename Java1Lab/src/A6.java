bcd0ce6ae9c7f111d43b7ca8c4d8185b9d2ca4fa
supersaxar <vfqkbnnkgjyb@gmail.com>
2024-10-12 10:11:16 +0800

    1 Laba sdelana
    slozna slozna nichego ne ponyatna

---
+++ Java1Lab/src/A6.java
@@ -0,0 +1,7 @@
+public class A6 {
+    public static void main(String[] args) {
+        System.out.println("Фамилия разработчика: Доржиев");
+        System.out.println("Дата получения задания: 14 сентября 2024 года");
+        System.out.println("Дата сдачи задания: 28 сентября 2024 года");
+    }
+}
