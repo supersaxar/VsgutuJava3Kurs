01725578840ac83fb219bd4ff77e6387d4c14115
supersaxar <vfqkbnnkgjyb@gmail.com>
2024-10-12 10:24:59 +0800

    Delete Java1Lab directory

--- Java1Lab/src/A6.java
+++
@@ -1,7 +0,0 @@
-public class A6 {
-    public static void main(String[] args) {
-        System.out.println("Фамилия разработчика: Доржиев");
-        System.out.println("Дата получения задания: 14 сентября 2024 года");
-        System.out.println("Дата сдачи задания: 28 сентября 2024 года");
-    }
-}
