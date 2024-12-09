3a79d84947a35e79b04a88a4b1d153292ae819ad
supersaxar <vfqkbnnkgjyb@gmail.com>
2024-12-09 18:58:27 +0800

    Update ArrayListJosephusTest.java

--- Java 11 Лаба/src/test/java/DorzhievZhargalB7621/B/ArrayListJosephusTest.java
+++ Java 11 Лаба/src/test/java/DorzhievZhargalB7621/B/ArrayListJosephusTest.java
@@ -3,15 +3,25 @@ package DorzhievZhargalB7621.B;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 public class ArrayListJosephusTest {
     @Test
     public void testValidInput() {
-        assertEquals(1, JosephusProblem.solveWithArrayList(1));
-        assertEquals(1, JosephusProblem.solveWithArrayList(8));
+        ArrayList<Integer> arrayListCircle = new ArrayList<>();
+        arrayListCircle.add(1);
+        assertEquals(1, JosephusProblem.solveJosephusProblem(arrayListCircle));
+
+        arrayListCircle.clear();
+        for (int i = 1; i <= 8; i++) {
+            arrayListCircle.add(i);
+        }
+        assertEquals(1, JosephusProblem.solveJosephusProblem(arrayListCircle));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testInvalidInput() {
-        JosephusProblem.solveWithArrayList(0);
+        ArrayList<Integer> emptyList = new ArrayList<>();
+        JosephusProblem.solveJosephusProblem(emptyList);
     }
 }
